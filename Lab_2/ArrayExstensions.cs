57b0bc93c71fc78265423fb056fe20a7b84dd5a4
Mykola Borshulyak <borshuliakm@gmail.com>
2023-10-10 21:01:16 +0300

    First task done

---
+++ Lab_2/ArrayExstensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab_2;
+
+public static class ArrayExstensions
+{
+    public static int[] SortAsc(this int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j - 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+        return arr;
+    }
+}
+
--- Labs/Program.cs
+++ Labs/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Lab_2;
 Console.WriteLine("Hello, World!");
+int[] ints = new int[] { 1, 5, 4, 2, 3, 8, 4, 2, 1, 5, 6, 8, 9, 77, 11, 22 };
+ints.SortAsc();
+Console.WriteLine(ints[8]);
