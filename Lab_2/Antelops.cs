6353cb3825d7620cf77dc7e87a388c021f5fdf77
Mykola Borshulyak <borshuliakm@gmail.com>
2023-10-10 23:11:44 +0300

    .

---
+++ Lab_2/Antelops.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab_2;
+
+public class Antelops
+{
+    private Antelope[] Antelopes;
+
+    public Antelops()
+    {
+        Antelopes = new Antelope[];
+    }
+
+    public Add_Antelope(Antelop antelop) => Antelopes.Add();
+    public Remove_Antelope_by_Id(int id) => Antelopes.remove(id);
+    public 
+}
--- Labs/Program.cs
+++ Labs/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Lab_2;
-Console.WriteLine("Hello, World!");
+
+/*Console.WriteLine("Hello, World!");
 int[] ints = new int[] { 1, 5, 4, 2, 3, 8, 4, 2, 1, 5, 6, 8, 9, 77, 11, 22 };
 ints.SortAsc();
-Console.WriteLine(ints[8]);
+Console.WriteLine(ints[8]);*/
+Antelope Jeiran = new("Джейран", "А", "Азія", 30000);
+Antelope Gnu = new("Гну", "В", "Африка", 560000);
+Antelope Beiza = new("Бейзя", "Н", "Африка", 2500);
+Antelope Antelope = new();
