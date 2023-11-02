608a18f48931020e7689aa0afc4389d1e79e9b5e
BorshuliakM <borshuliakm@gmail.com>
2023-11-03 01:58:28 +0200

    .

--- Lab_10/CharList.cs
+++ Lab_10/CharList.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Lab_10;
 
-namespace Lab_10
+public class CharList
 {
-    internal class CharList
+    private List<char> elements = new List<char>();
+    public void Add(char element)
     {
+        elements.Add(element);
+    }
+    public void RemoveAt(int k)
+    { 
+        elements.RemoveAt(k); 
+    }
+    public static CharList operator +(CharList left, CharList right)
+    {
+        CharList newList = new CharList();
+        foreach (char element in left.elements)
+        {
+            newList.Add(element);
+        }
+        foreach (char element in right.elements)
+        {
+            newList.Add(element);
+        }
+        return newList;
+    }
+    public static CharList operator --(CharList lst)
+    {
+        CharList newList = new CharList();
+        foreach (char element in lst.elements)
+        {
+            newList.Add(element);
+        }
+        newList.RemoveAt(0);
+        return newList;
+    }
+    public static bool operator ==(CharList left, CharList right)
+    {
+        if (ReferenceEquals(left, right))
+            return true;
+        if (left is null || right is null)
+            return false;
+        return left.elements.Equals(right.elements);
+    }
+    public static bool operator !=(CharList left, CharList right)
+    {
+        return !(left == right);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is CharList otherList)
+            return this == otherList;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return elements.GetHashCode();
     }
 }
--- Lab_10/Vector.cs
+++ Lab_10/Vector.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Lab_10;
 
-namespace Lab_10
+class Vector
 {
-    internal class RealArray
+    private double[] values;
+
+    public Vector(int length)
     {
+        values = new double[length];
+    }
+
+    public Vector(double[] values)
+    {
+        this.values = values;
+    }
+
+    public int Length
+    {
+        get { return values.Length; }
+    }
+
+    public double this[int index]
+    {
+        get { return values[index]; }
+        set { values[index] = value; }
+    }
+
+    public static Vector operator +(Vector v1, Vector v2)
+    {
+        if (v1.Length != v2.Length)
+            throw new ArgumentException("Vectors must have the same length");
+
+        Vector result = new Vector(v1.Length);
+        for (int i = 0; i < v1.Length; i++)
+        {
+            result[i] = v1[i] + v2[i];
+        }
+        return result;
+    }
+    public static Vector operator +(Vector v, double number)
+    {
+        Vector result = new Vector(v.Length);
+        for (int i = 0; i < v.Length; i++)
+        {
+            result[i] = v[i] + number;
+        }
+        return result;
     }
 }
